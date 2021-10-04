77cacac04daed623a2079369eb592164e8ae4bd1
MrJanHorak <janny_b_goode@hotmail.com>
2021-10-04 10:53:00 -0400

    fix setRange input to ensure only numbers are accepted

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const game = {
   secretNum: null,
   prevGuesses: [],
 
-  // prompts the user for thier guess 
+  // prompts the user for their guess 
   getGuess(){
     let input=0
     while (input < this.smallestNum || input>this.biggestNum || Number.isNaN(input)){
@@ -16,7 +16,7 @@ const game = {
     return input
   },
 
-//creates output alert to inform the user if thier vote hit the number or was too high or low
+//creates output alert to inform the user if their vote hit the number or was too high or low
   render(guess){
     if (guess===this.secretNum){window.alert(`Congrats! You guessed the number in ${this.prevGuesses.length} turns!`)
     } else {
@@ -39,15 +39,18 @@ const game = {
 
   // request user input to set the guessing range
   setRange() {
+      do{
       this.smallestNum = parseInt(prompt(`Please enter the lowest number you would like to guess: `))
+      } while (Number.isNaN(this.smallestNum))
+      do {
       this.biggestNum = parseInt(prompt(`Please enter the highest number you would like to guess: `))
+      } while (Number.isNaN(this.biggestNum))
     },
 
   // sets the game sequence into motion when called
   play: function() {
     this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
     let input;
-    console.log(this.secretNum)
     while (input!==this.secretNum){
       input = this.getGuess()
       this.adjustMinMax(input)
